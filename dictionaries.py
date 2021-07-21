294c35926239a8a6ee11f0545ffc52191f806bc0
unknown <kothamnaideep@123gmail.com>
2021-07-21 12:13:00 +0530

    listtupledictionaried

---
+++ dictionaries.py
@@ -0,0 +1,9 @@
+students={"name":"manideep","age":21,"company":"prodapt"}
+students["age"]=23
+del students["age"]
+print(len(students))
+print(students.keys())
+print(students.values())
+
+print(students)
+students.clear()
