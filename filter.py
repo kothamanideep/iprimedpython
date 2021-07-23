231034ee5cf69425fb3618ccc7515df51507c784
unknown <kothamnaideep@123gmail.com>
2021-07-23 10:29:55 +0530

    days 5

---
+++ filter.py
@@ -0,0 +1,3 @@
+l1=[10,20,30,40]
+l2=[10,20,67,75]
+print(list(filter(lambda x:x in l1,l2)))
--- functions.py
+++ functions.py
@@ -1,8 +1,8 @@
 # def sayhello():         #function definition
 #     return "hello"
 # print(sayhello())        #function calling
-def sayhello(name):
-    return "hello" +name
-getname=input("entername:")
-print(sayhello(getname))
+# def sayhello(name):
+#     return "hello" +name
+# getname=input("entername:")
+# print(sayhello(getname))
 
