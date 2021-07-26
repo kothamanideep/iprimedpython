5a9355409f7ded33566eeb18997c0b6f9def8d90
unknown <kothamnaideep@123gmail.com>
2021-07-26 12:01:53 +0530

    car

--- day6tasks/car.py
+++ day6tasks/car.py
@@ -2,13 +2,25 @@ class Cars:
     color="white"
     brand="BMW"
 
+    def findMileage(self,li,km):
+        return km/li
+
+
+
 myCar=Cars()
 a1=input("enter color:")
 a2=input("enter brand :")
+
+l=int(input("enter liters:"))
+k=int(input("enter kilometers:"))
+mileage=myCar.findMileage(l,k)
+
+
 myCar.color=a1
 myCar.brand=a2
 print(myCar.color)
 print(myCar.brand)
+print(mileage)
 defaultCar=Cars()
 print(defaultCar.color)
 print(defaultCar.brand)
