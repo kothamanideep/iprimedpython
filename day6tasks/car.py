c61bf6c364a2993a5f3c1983591a7101d38bb3ce
unknown <kothamnaideep@123gmail.com>
2021-07-26 11:53:05 +0530

    car

--- day6tasks/car.py
+++ day6tasks/car.py
@@ -3,7 +3,14 @@ class Cars:
     brand="BMW"
 
 myCar=Cars()
+a1=input("enter color:")
+a2=input("enter brand :")
+myCar.color=a1
+myCar.brand=a2
 print(myCar.color)
 print(myCar.brand)
+defaultCar=Cars()
+print(defaultCar.color)
+print(defaultCar.brand)
 
 
